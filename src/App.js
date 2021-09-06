322e53b479793074c22d2070f2a82be65a3d02dc
Eljan Guluyev <77200703+eljn0@users.noreply.github.com>
2021-09-06 12:25:42 +0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+  
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = '06741424-0df4-46cc-8164-3427fb0de3d4';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
+  );
+};
+
+// Logout elemey ucun ctrl shift i sonra terminalda applicationa basin local serveri temizleyin
+
+export default App;
